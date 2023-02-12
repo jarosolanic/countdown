ffa66fccc17476033f6c65b3f33603fb03d28060
jarosolanic <55108417+jarosolanic@users.noreply.github.com>
2023-02-12 08:41:20 +0100

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     for (let _index = 0; _index <= 2; _index++) {
         music.playTone(262, music.beat(BeatFraction.Quarter))
         basic.showNumber(3 - _index)
